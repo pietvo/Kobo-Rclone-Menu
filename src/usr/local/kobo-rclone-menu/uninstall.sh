f07c2d7eb1690ed12c91fd08525dbbe2b48a4ea8
Pieter van Oostrum <pieter@vanoostrum.org>
2024-04-16 20:09:51 +0200

    Uninstall more files

--- src/usr/local/kobo-rclone-menu/uninstall.sh
+++ src/usr/local/kobo-rclone-menu/uninstall.sh
@@ -2,5 +2,12 @@
 
 # Uninstall kobo-rclone-menu
 
+#load config
+. $(dirname $0)/config.sh
+
 rm -f /etc/udev/rules.d/97-kobo-rclone-menu.rules
 rm -rf /usr/local/kobo-rclone-menu/
+rm -rf ${RCLONEDIR}
+rm -f ${MENUDIR}/rclone.menu
+rm -f $Logs/*.*
+rm -f $UserConfig
